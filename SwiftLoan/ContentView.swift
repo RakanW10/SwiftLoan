64a6e8bdb41717371c7144255c97848b4d32f3bd
rakan <Rakan.w10@outlook.com>
2023-10-28 11:50:13 +0300

    Initial Commit

---
+++ SwiftLoan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLoan
+//
+//  Created by Rakan Alotaibi on 13/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLoan/SwiftLoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLoanApp.swift
+//  SwiftLoan
+//
+//  Created by Rakan Alotaibi on 13/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
